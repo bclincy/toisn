bb5e258b543d1c7e7c50be15b418711044f65a8e
Jacory Armstrong <jacoryarmstrong@gmail.com>
2022-10-13 22:51:12 -0400

    commit new javascript buttton background color changes

--- index.js
+++ index.js
@@ -106,3 +106,39 @@ function closeHero(){
 		x.style.display = "none";
 	}
 }
+//adding sweetAlerts 
+
+// Footer 
+$(document).ready(function($) {
+
+  $(window).on('scroll', function() {
+
+    //ADD .TIGHT
+    if ($(window).scrollTop() + $(window).height() > $('.wrapper').outerHeight()) {
+      $('body').addClass('tight');
+      $('.arrow').hide();
+    } else {
+      $('body').removeClass('tight');
+      $('.arrow').show();
+    }
+  });
+
+  //BACK TO PRESENTATION MODE
+  $("html").on("click", "body.tight .wrapper", function() {
+    $('html, body').animate({
+      scrollTop: $('.wrapper').outerHeight() - $(window).height()
+    }, 500);
+  });
+
+});
+
+$('.arrow').click(function(){
+   $("html").animate({ scrollTop: $('html').prop("scrollHeight")}, 1200);
+});
+
+//footer 
+$(function() {
+  $('.footer-links-holder h3').click(function () {
+    $(this).parent().toggleClass('active');
+  });
+});
